2aa13caa03baec13d5102b6d59d75a9d1256b32e
choidongkuen <danaver12@daum.net>
2023-02-04 13:00:40 +0900

    feat : AOP LockService 에러발생

---
+++ src/main/java/com/example/accountsystemimpl/service/LockService.java
@@ -0,0 +1,13 @@
+package com.example.accountsystemimpl.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class LockService {
+
+    private final RedissonClient redissonClient;
+}
