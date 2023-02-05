bdc88628138afb642db52a6d9d372d7e487ae739
choidongkuen <danaver12@daum.net>
2023-02-05 23:57:47 +0900

    2/5

---
+++ src/main/java/com/example/accountsystemimpl/service/LockAopAspect.java
@@ -0,0 +1,16 @@
+package com.example.accountsystemimpl.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class LockAopAspect {
+
+    @Around("@")
+}
